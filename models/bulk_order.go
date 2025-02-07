fd7918d6a14f2fe46d5ad2b19596a0775cbb5240
aryan-gupta-omniful <aryan.gupa@omniful.ai>
2025-02-07 15:27:50 +0530

    Parse CSV + Validate Order

--- models/bulk_order.go
+++ models/bulk_order.go
@@ -1,5 +1,32 @@
 package models
 
+import "time"
+
+type Order struct {
+	ID              string      `bson:"_id,omitempty" json:"order_id"`
+	CustomerID      string      `bson:"customer_id" json:"customer_id"`
+	CreatedAt       time.Time   `bson:"created_at" json:"created_at"`
+	Currency        string      `bson:"currency" json:"currency"`
+	TotalAmount     int         `bson:"total_amount" json:"total_amount"`
+	TransactionID   string      `bson:"transaction_id" json:"transaction_id"`
+	OrderItems      []OrderItem `bson:"order_items" json:"order_items"`
+	ModeOfPayment   string      `bson:"mode_of_payment" json:"mode_of_payment"`
+	Status          string      `bson:"status" json:"status"`
+	BillingAddress  interface{} `bson:"billing_address" json:"billing_address"`
+	ShippingAddress interface{} `bson:"shipping_address" json:"shipping_address"`
+	InvoiceID       interface{} `bson:"invoice_id" json:"invoice_id"`
+	TenantID        interface{} `bson:"tenant_id" json:"tenant_id"`
+}
+
+type OrderItem struct {
+	ID              string `bson:"_id,omitempty" json:"order_items_id"`
+	OrderID         string
+	SKUID           string `bson:"sku_id" json:"sku_id"`
+	QuantityOrdered int    `bson:"quantity_ordered" json:"quantity_ordered"`
+	HubID           string `bson:"hub_id" json:"hub_id"`
+	SellerID        string `bson:"seller_id" json:"seller_id"`
+}
+
 type BulkOrderQueueMessage struct {
 	CustomerID string `json:"customer_id"`
 	FilePath   string `json:"file_path"`
--- models/customer.go
+++ models/customer.go
@@ -31,30 +31,6 @@ type Address struct {
 	Country      string `bson:"country" json:"country"`
 }
 
-type OrderItem struct {
-	ID              string `bson:"_id,omitempty" json:"order_items_id"`
-	SKUID           string `bson:"sku_id" json:"sku_id"`
-	QuantityOrdered int    `bson:"quantity_ordered" json:"quantity_ordered"`
-	HubID           string `bson:"hub_id" json:"hub_id"`
-	SellerID        string `bson:"seller_id" json:"seller_id"`
-}
-
-type Order struct {
-	ID              string      `bson:"_id,omitempty" json:"order_id"`
-	CustomerID      string      `bson:"customer_id" json:"customer_id"`
-	CreatedAt       time.Time   `bson:"created_at" json:"created_at"`
-	Currency        string      `bson:"currency" json:"currency"`
-	TotalAmount     int         `bson:"total_amount" json:"total_amount"`
-	TransactionID   string      `bson:"transaction_id" json:"transaction_id"`
-	OrderItems      []OrderItem `bson:"order_items" json:"order_items"`
-	ModeOfPayment   string      `bson:"mode_of_payment" json:"mode_of_payment"`
-	Status          string      `bson:"status" json:"status"`
-	BillingAddress  interface{} `bson:"billing_address" json:"billing_address"`
-	ShippingAddress interface{} `bson:"shipping_address" json:"shipping_address"`
-	InvoiceID       interface{} `bson:"invoice_id" json:"invoice_id"`
-	TenantID        interface{} `bson:"tenant_id" json:"tenant_id"`
-}
-
 type Credentials struct {
 	EntityID     string    `bson:"entity_id" json:"entity_id"`
 	EntityType   string    `bson:"entity_type" json:"entity_type"`
